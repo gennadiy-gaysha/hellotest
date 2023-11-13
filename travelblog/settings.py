d5468839c138b00aab84dea47ab8f3b88d0e9c83
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-11-13 17:41:35 +0100

    404

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -16,7 +16,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["127.0.0.1", "hello-test-gaysha-568bd4e6235c.herokuapp.com"]
 
